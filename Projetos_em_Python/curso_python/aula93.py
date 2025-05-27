49a4f388945499a61719de7c5ef0c2e7075e1d61
Hugo Pereira <hugoper_cs@yahoo.com>
2025-05-27 14:56:58 -0300

    atualiza aula 93

--- Projetos_em_Python/curso_python/aula93.py
+++ Projetos_em_Python/curso_python/aula93.py
@@ -14,8 +14,11 @@ except ZeroDivisionError: # trata o erro de divisão por zero
     print("Dividiu por zero")
 except NameError: # trata o erro de nome não definido
     print("Nome b não está definido")
-except (TypeError, IndexError): # também é possível tratar mais de um erro na mesma linha 
+except (TypeError, IndexError) as error: # também é possível tratar mais de um erro na mesma linha. as error cria uma variável com a instãncia do erro
+    # OBS.: Faz mais sentido tratar um erro de cada vez caso vc queira saber qual erro ecorreu.
     print("TypeError + IndexError")
+    print("MSG:", error)
+    print("Nome:", error.__class__.__name__) # Mostra especificamente a classe do erro ocorrido
 except Exception: # Exception é a classe superior, tarta qualquer erro que possa ocorrer
     print("ERRO DESCONHECIDO.")
 
